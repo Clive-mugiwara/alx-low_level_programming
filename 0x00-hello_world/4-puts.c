ddcd7232a5c6120375e23b1dcbcd90fd85726b28
Clive <daeclive@gmail.com>
2023-02-17 17:18:27 -0800

    C task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-    char Mystr[] ="\"Programming is like building a multilingual puzzle";
-
-    puts(Mystr); 
-
-    return 0;
+puts("\"Programming is like building a multilingual puzzle"); 
+return (0);
 }
-  
