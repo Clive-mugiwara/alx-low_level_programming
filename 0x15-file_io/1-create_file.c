f6eb5bbb7bfee8c034ab9bd8e641f29d961ab8e3
Clive Dei <daeclive@gmail.com>
2023-04-11 15:49:58 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_file- creates a file 
+ * create_file- creates a file
  * @filename: Pointer for the filename of the file created
  * @text_content: Pointer to string that is written to the file
  *
@@ -20,21 +20,19 @@ return (1);
 
 if (text_content != NULL)
 {
-	for(l = 0; text_content[l];)
+	for (l = 0; text_content[l];)
 		l++;
 
 }
 
-file_d =open(filename, O_CREAT| O_RDWR |O_TRUNC, 0600);
-by_w =write(file_d,text_content,l);
+file_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+by_w = write(file_d, text_content, l);
 
 if (file_d == -1 || by_w == -1)
-	return(-1);
+	return (-1);
 
-
-
-close (file_d);
+close(file_d);
 
 return (1);
 
-	}	
+}
