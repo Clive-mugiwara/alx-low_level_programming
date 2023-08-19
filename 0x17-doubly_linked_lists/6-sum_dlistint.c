2085f063fd31e243e9b3b958defccdff7bf12ce9
root <daeclive@gmail.com>
2023-08-18 17:53:58 -0700

    Sum list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum of all nodes of a dlistint_t list
+ * @head: head node of list to count
+ * Return: nth node
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp = NULL;
+	size_t sum = 0;
+
+	if (!head)
+		return (sum);
+	tmp = head;
+	while (tmp->prev)
+		tmp = tmp->prev;
+	for (; tmp; tmp = tmp->next)
+		sum += tmp->n;
+	return (sum);
+}
